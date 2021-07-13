3e5efdc46375c33a8bdcd4ca788ebcd0f1acb2e1
Raphael Souza de Oliveira <raphaelsouzadeoliveira@MacBook-Pro-de-Raphael.local>
2021-07-13 11:52:02 -0300

    Transformando o Password em Hash

--- app/models/usuarios.js
+++ app/models/usuarios.js
@@ -1,10 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 let usuarioSchema = new mongoose.Schema({
-    nome: String,
-    email: { String, required: true, unique: true },
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now }
-})
+  nome: String,
+  email: { String, required: true, unique: true },
+  password: { type: String, required: true },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+});
 
-module.exports = mongoose.model('Usuario', usuarioSchema);
+usuarioSchema.pre("save", function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const document = this;
+    bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+      if (err) next(err);
+      else {
+        this.password = hashedPassword;
+        next();
+      }
+    });
+  }
+});
+
+module.exports = mongoose.model("Usuario", usuarioSchema);
